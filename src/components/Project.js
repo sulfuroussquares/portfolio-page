67b9f48d49c30e0b6bbd3ecf195edab7b8354186
BRIAN BETANCOURT <brianbetancourt@BRIANs-MacBook-Pro.local>
2020-12-25 09:04:41 -0700

    merry Christmas! updated project component

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,8 +1,62 @@
 import React, {useEffect, useState} from "react";
+// import project from "../portfoliostudio/schemas/project.js";
 
 import sanityClient from "../client.js";
 
 export default function Project() {
 
-    return <h1>Project Page</h1>
+    const [projectData, setProjectData] = useState(null);
+
+    useEffect(() => {
+        sanityClient.fetch(`*[_type == "project"]{
+            title,
+            date,
+            place,
+            description,
+            projectType,
+            link,
+            tags
+        }`).then((data) => setProjectData(data))
+        .catch((e)=>console.log(e));
+    }, []);
+
+
+
+    return (
+        <main className="bg-green-100 min-h-screen p-12"> 
+            <section className="container mx-auto">
+                {/* Page Title */}
+                <h1 className="text-5xl flex justify-center cursive"> My Projects </h1>
+                {/* Subtitle */}
+                <h2 className="text-lg text-gray-600 flex justify-center mb-12"> Welcome to my projects page.</h2>
+                <section className="grid grid-cols-2 gap-8">
+                    {projectData && projectData.map((project, index) => (
+                    <article className="relative rounded-lg shadow-xl bg-white p-16">
+                        {/* Project Title with clickable link to parameteres from Sanity studio */}
+                        <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-red-700">
+                            <a href={project.link} alt={project.title} target="_blank" rel="noopener noreferrer"> {project.title}</a>
+                        </h3>
+                        <div className="text-gray-500 text-xs space-x-4">
+                            {/* Project Information */}
+                            <span>
+                                <strong className="font-bold">Finished on</strong> : {" "} {new Date(project.date).toLocaleDateString()}
+                            </span>
+                            <span>
+                                <strong className="font-bold">Company </strong> : {" "} {project.place}
+                            </span>
+                            <span>
+                                <strong className="font-bold">Type </strong>: {" "} {project.type}
+                                {project.projectType}
+                            </span>
+                            <p className="my-6 text-lg text-gray-700 leading-relaxed">{project.description}</p>
+                            <a href={project.link} rel="noopener noreferrer" target="_blank"
+                            className="text-red-500 font-bold hover:underline hover:text-red-400 text-xl"> View the Project {" "}
+                            <span role="img" aria-label="right-pointer"> 👉 </span></a>
+                        </div>
+                    </article>
+                    ))}
+                </section>
+            </section>
+        </main>
+    )
 }
--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,6 +1,27 @@
 import React from "react";
+import {useParams} from "react-router-dom";
+import sanityClient from "../client.js";
+// using image url builder from @sanity/image-url package (npm install @sanity/image-url)
+import imageUrlBuilder from "@sanity/image-url";
+
+
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+    return builder.image(source);
+}
+
+
 
 export default function SinglePost() {
+    const [singlePost, setSinglePost] = useState(null);
+    // slugs are generated and unique for each post. Here, we use the useParam function to retrieve the slug
+    // (a-slug-looks-like-this)
+    const {slug} = useParam();
 
-    return <h1>SinglePost Page</h1>
+    // uses GROQ query in fetch function
+    useEffect(() => {
+        sanityClient.fetch(`*[slug.current == "${slug}"]`)
+    })
+
+    return ()
 }
