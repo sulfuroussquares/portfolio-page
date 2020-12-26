a2e49491dcada4ca3f4741ee0ce5b6f7f1b75767
BRIAN BETANCOURT <brianbetancourt@BRIANs-MacBook-Pro.local>
2020-12-26 06:56:01 -0700

    added about me component

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,51 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import sanityClient from "../client.js";
+import background from "../unsplash.jpg"
+import imageUrlBuilder from "@sanity/image-url";
+import BlockContent from "@sanity/block-content-to-react";
+
+
+
+// Copied from singlepost component
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+    return builder.image(source);
+}
+
 
 export default function About() {
 
-    return <h1>About Page</h1>
-}
+    // set up state
+    const [author, setAuthor] = useState(null);
+    useEffect( () => {
+        // GROQ syntax in fetch() method
+        sanityClient.fetch(`*[_type == "author"]{
+            name,
+            bio,
+            "authorImage" : image.asset->url
+        }`).then((data) => setAuthor(data[0]))
+        .catch((e) => console.log(e));
+
+    }, []);
+
+    // if no author, return "loading...."
+    if (!author) return <div>Loading...</div>
+
+    return (
+        <main className="relative ">
+            <img src={background} alt="mountains" className="absolute w-full"></img>
+            <div className="p-10 lg:pt-48 container mx-auto relative">
+                <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
+                    <img src={urlFor(author.authorImage).url()} className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8" alt={author.name}/>
+                    <div className="text-lg flex flex-col justify-center">
+                        <h1 className="cursive text-6xl text-green-300 mb-4">
+                            Hey there. I'm {" "}
+                            <span className="text-green-100">{author.name}</span>
+                        </h1>
+                        <div className="prose lg:prose-xl text-white"> <BlockContent blocks={author.bio} projectId="fzks5u4e" dataset="production"/> </div>
+                    </div>
+                </section>
+            </div>
+        </main>
+    )
+} // end About()
